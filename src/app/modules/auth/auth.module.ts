9b8b126fb973670657dda78aac3039c4ca1be065
pierrecampoverde <pierrecampoverde@gmail.com>
2024-01-07 13:04:09 -0300

    Sign up form created

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -4,11 +4,12 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RegisterComponent } from './components/register/register.component';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 
 @NgModule({
   declarations: [
-  
+
   ],
   imports: [
     CommonModule,
@@ -16,6 +17,9 @@ import { RegisterComponent } from './components/register/register.component';
     HttpClientModule,
     LoginComponent,
     RegisterComponent
+  ],
+  providers: [
+    {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'outline'}}
   ]
 })
 export class AuthModule { }
--- src/app/modules/auth/components/register/register.component.ts
+++ src/app/modules/auth/components/register/register.component.ts
@@ -1,11 +1,89 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { ThirdPartyAuthService } from '../../services/third-party-auth.service';
+import { Router } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { CommonModule } from '@angular/common';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.sass'],
-  standalone : true
+  standalone: true,
+  providers: [AuthService, ThirdPartyAuthService],
+  imports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    CommonModule,
+  ],
 })
 export class RegisterComponent {
+  constructor(
+    private fb: FormBuilder,
+    private service: AuthService,
+    private thirdpartyAuth: ThirdPartyAuthService,
+    private router: Router
+  ) {}
 
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  passwordFormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(8),
+    Validators.maxLength(16),
+  ]);
+
+  repeatPasswordFormControl = this.fb.control('', [
+    Validators.required,
+    this.passwordMatchValidator(),
+  ]);
+
+  checkboxFormControl = new FormControl(false, [Validators.requiredTrue]);
+
+  isFormSubmitted = false;
+
+  navigate(param: string) {
+    this.router.navigate([param]);
+  }
+
+  private passwordMatchValidator() {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const password = this.passwordFormControl.value;
+      const repeatPassword = control.value;
+
+      return password === repeatPassword ? null : { mismatch: true };
+    };
+  }
+
+  onSubmit() {
+    this.isFormSubmitted = true;
+
+    // Additional logic for form submission
+    if (
+      this.emailFormControl.valid &&
+      this.passwordFormControl.valid &&
+      this.repeatPasswordFormControl.valid &&
+      this.checkboxFormControl.valid
+    ) {
+      // Perform registration or other actions
+      console.log('Form submitted successfully!');
+    }
+  }
 }
