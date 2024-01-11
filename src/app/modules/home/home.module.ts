5175fb7de758640bbf1109a01af6fae5d3b6fb65
Adrian Mendoza Mamani <adrian.mendoza@fundacion-jala.org>
2024-01-11 15:56:23 -0400

    TG-35 Design: Add Sidebar in Home page

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { HttpClientModule } from '@angular/common/http';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './components/home.component';
-import { HttpClientModule } from '@angular/common/http';
-import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [MatButtonModule, CommonModule, HomeRoutingModule, HttpClientModule],
+  imports: [
+    MatButtonModule,
+    CommonModule,
+    HomeRoutingModule,
+    HttpClientModule,
+    MatSidenavModule,
+    MatListModule,
+    MatToolbarModule,
+  ],
 })
 export class HomeModule {}
