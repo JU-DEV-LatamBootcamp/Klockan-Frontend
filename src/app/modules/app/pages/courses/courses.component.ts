455f3b00c92feeead458ca1bb752cf11e15fde08
Gary Sanchez <ronald.sanchez@jala.university>
2024-01-26 10:35:11 -0400

    Feature: Enabled Courses list

--- src/app/modules/app/pages/courses/courses.component.ts
+++ src/app/modules/app/pages/courses/courses.component.ts
@@ -1,8 +1,36 @@
 import { Component } from '@angular/core';
+import { Course } from 'src/app/shared/models/Courses';
+import { CourseService } from 'src/app/shared/services/course.service';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.sass'],
 })
-export class CoursesComponent {}
+export class CoursesComponent {
+  headers = ['id', 'name', 'description'];
+  data: Course[] | Course | null | any = [];
+
+  constructor(public courseService: CourseService) {
+    this.fetchData();
+  }
+
+  fetchData() {
+    try {
+      this.courseService.getAll().subscribe(
+        (data: Course[] | Course | null) => {
+          this.data = data;
+        },
+        error => {
+          console.log(
+            'Something went wrong while trying to retrieve data from API',
+            error
+          );
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+}
