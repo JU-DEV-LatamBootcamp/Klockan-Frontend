79f270dbd47786469eda52a08ed6c810e391ee89
Andres Nazzari <andres.nazzari@jala.university>
2024-02-01 17:15:13 -0300

    Refactor course service to use new endpoints TG-93

--- src/app/shared/services/course.service.ts
+++ src/app/shared/services/course.service.ts
@@ -5,21 +5,22 @@ import { map } from 'rxjs/operators';
 import { BaseService } from './base.service';
 import { transformCourseFromService } from '../utils/course-mapper';
 import { Course, CourseFromService } from '../models/Courses';
-import { environment } from 'src/environments/keycloak.enviroment';
+import { Endpoints } from '../constants/EndpointsEnum';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CourseService extends BaseService<Course> {
-  coursePath = environment.api.coursesEndpoint;
-
   override getAll(): Observable<Course[]> {
     const token = this.oAuthService.getAccessToken();
     const headers = super.createHeaders(token);
     return this.http
-      .get<CourseFromService[]>(`${this.baseRoute}${this.coursePath}`, {
-        headers,
-      })
+      .get<CourseFromService[]>(
+        `${this.baseRoute}${Endpoints.GET_ALL_COURSES}`,
+        {
+          headers,
+        }
+      )
       .pipe(
         map(coursesFromService =>
           transformCourseFromService(coursesFromService)
@@ -31,8 +32,15 @@ export class CourseService extends BaseService<Course> {
     alert('Editing COURSE' + entity.name);
     throw new Error('Method not implemented.');
   }
-  override delete(entity: Course): Observable<Course> {
-    alert('Deleting COURSE' + entity.id);
-    throw new Error('Method not implemented.');
+
+  override delete(entity: Course): Observable<any> {
+    const token = this.oAuthService.getAccessToken();
+    const headers = super.createHeaders(token);
+    console.log('first');
+    // alert('Deleting COURSE' + entity.id);
+    return this.http.delete(
+      `${this.baseRoute}${Endpoints.DELETE_COURSE}/${entity.id}`,
+      { headers }
+    );
   }
 }
