d3a92b34d937ebf96c46605c99623a3906137ad5
carlosBarriosCarvajal <Carlos.Barrios@jala.university>
2024-02-02 18:53:17 -0500

    Add return tipe for dialog service TG-92

--- src/app/shared/layouts/app-layout/services/dialog/dialog.service.ts
+++ src/app/shared/layouts/app-layout/services/dialog/dialog.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, TemplateRef } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ComponentType } from '@angular/cdk/portal';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: null,
@@ -8,7 +9,10 @@ import { ComponentType } from '@angular/cdk/portal';
 export class DialogService {
   constructor(private readonly dialog: MatDialog) {}
 
-  show<T>(component: ComponentType<T> | TemplateRef<T>, dataFromEvent?: any) {
+  show<T>(
+    component: ComponentType<T> | TemplateRef<T>,
+    dataFromEvent?: any
+  ): Observable<any> {
     // IMPROVEMENT: should receive the component and wrap it with the dialog component, if its a small screen then open the component in the panel
     const dialogRef = this.dialog.open(component, {
       panelClass: 'dialog__container',
