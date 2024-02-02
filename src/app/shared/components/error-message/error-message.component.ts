6057bfe832e49920fec588531729a6aaddc5b1e7
Andres Nazzari <andres.nazzari@jala.university>
2024-02-02 14:35:22 -0300

    Remove unused imports and variables in ErrorMessageComponent TG-93

--- src/app/shared/components/error-message/error-message.component.ts
+++ src/app/shared/components/error-message/error-message.component.ts
@@ -1,7 +1,5 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { IDeleteConfirmationData } from '../../interfaces/IDeleteConfirmationData';
-import { Subscription, Observable } from 'rxjs';
 import { DialogService } from 'src/app/shared/layouts/app-layout/services/dialog/dialog.service';
 
 @Component({
@@ -10,9 +8,6 @@ import { DialogService } from 'src/app/shared/layouts/app-layout/services/dialog
   styleUrls: ['./error-message.component.sass'],
 })
 export class ErrorMessageComponent {
-  subscription!: Subscription;
-  itemDeleted$!: Observable<any>;
-
   constructor(
     public dialogRef: MatDialogRef<ErrorMessageComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
