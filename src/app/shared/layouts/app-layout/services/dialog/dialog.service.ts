8c2fab1e12863d15737efa4042e0d239fb0755a8
Andres Nazzari <andres.nazzari@jala.university>
2024-02-02 13:26:00 -0300

    Improve DialogService to return the afterClosed() value TG-93

--- src/app/shared/layouts/app-layout/services/dialog/dialog.service.ts
+++ src/app/shared/layouts/app-layout/services/dialog/dialog.service.ts
@@ -10,10 +10,11 @@ export class DialogService {
 
   show<T>(component: ComponentType<T> | TemplateRef<T>, dataFromEvent?: any) {
     // IMPROVEMENT: should receive the component and wrap it with the dialog component, if its a small screen then open the component in the panel
-    this.dialog.open(component, {
+    const dialogRef = this.dialog.open(component, {
       panelClass: 'dialog__container',
       maxWidth: 'auto',
       data: dataFromEvent,
     });
+    return dialogRef.afterClosed();
   }
 }
