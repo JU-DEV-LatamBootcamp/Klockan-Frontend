fe42b313534cfcf9adeddff5efc2cd12a97db139
Adrian Mendoza Mamani <adrian.mendoza@fundacion-jala.org>
2024-02-05 15:25:35 -0400

    TG-86: Add Create Form

---
+++ src/app/modules/app/pages/programs/components/program-form/program-form.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { ProgramService } from 'src/app/shared/services/program.service';
+
+@Component({
+  selector: 'app-program-form',
+  templateUrl: './program-form.component.html',
+  styleUrls: ['./program-form.component.sass'],
+  providers: [
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: { appearance: 'outline', floatLabel: 'always' },
+    },
+  ],
+})
+export class ProgramFormComponent implements OnInit {
+  programForm!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private dialogRef: MatDialogRef<ProgramFormComponent>,
+    private programService: ProgramService
+  ) {}
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    this.programForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.maxLength(200)]],
+      description: [''],
+    });
+  }
+
+  getFieldError(field: string): string | null {
+    const control = this.programForm.get(field);
+    if (control?.hasError('required')) {
+      return 'This field is required.';
+    } else if (control?.hasError('maxlength')) {
+      const errors = control.errors ? control.errors['maxlength'] : null;
+      const maxLength = errors ? errors.requiredLength : 0;
+      return `This field cannot be longer than ${maxLength} characters.`;
+    }
+    return null;
+  }
+
+  onSubmit(): void {
+    if (this.programForm.valid) {
+      this.programService.create(this.programForm.value).subscribe({
+        next: program => {
+          this.dialogRef.close(program);
+        },
+        error: error => {
+          console.error('Error al crear el programa:', error);
+        },
+      });
+    } else this.programForm.markAllAsTouched();
+  }
+}
