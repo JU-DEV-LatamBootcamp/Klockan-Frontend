8f239a3de1173b6175c23a24792d87a74c2d6a31
RamiroJGuillen <ramiro.guillen@jala.university>
2024-02-06 13:31:47 -0300

    refactored code

--- src/app/modules/app/pages/courses/components/course-form/course-form.component.ts
+++ src/app/modules/app/pages/courses/components/course-form/course-form.component.ts
@@ -1,10 +1,9 @@
-import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
-import { Observable, Subscription } from 'rxjs';
 
-import { Course, CourseToService } from 'src/app/shared/models/Courses';
+import { Course } from 'src/app/shared/models/Courses';
 import { CourseService } from 'src/app/shared/services/course.service';
 
 @Component({
@@ -18,26 +17,18 @@ import { CourseService } from 'src/app/shared/services/course.service';
     },
   ],
 })
-export class CourseFormComponent implements OnInit, OnDestroy {
+export class CourseFormComponent implements OnInit {
   courseForm!: FormGroup;
-  course$!: Observable<Course | CourseToService>;
-  courseSubscription!: Subscription;
 
   constructor(
-    private formBuilder: FormBuilder,
-    public courseService: CourseService,
-    private dialogRef: MatDialogRef<CourseFormComponent>,
+    private readonly formBuilder: FormBuilder,
+    private readonly courseService: CourseService,
+    private readonly dialogRef: MatDialogRef<CourseFormComponent>,
     @Inject(MAT_DIALOG_DATA) public data: Course
   ) {}
 
   ngOnInit(): void {
     this.initializeForm();
-  }
-
-  ngOnDestroy(): void {
-    if (this.courseSubscription) {
-      this.courseSubscription.unsubscribe();
-    }
   }
 
   initializeForm(): void {
@@ -50,38 +41,29 @@ export class CourseFormComponent implements OnInit, OnDestroy {
     });
   }
 
-  onSubmit(): void {
+  public onSubmit(): void {
     if (this.courseForm.valid) {
       if (this.data) {
-        this.editCourse(this.courseForm.value);
+        this.editCourse();
       } else {
-        this.createCourse(this.courseForm.value);
+        this.createCourse();
       }
-    }
+    } else this.courseForm.markAllAsTouched();
   }
 
-  private createCourse(courseData: Course): void {
-    const newCourse = {
-      name: courseData.name,
-      sessions: courseData.sessions,
-      duration: courseData.duration,
-      description: courseData.description,
-    };
-
-    this.course$ = this.courseService.create(newCourse);
-    this.courseSubscription = this.course$.subscribe({
+  private createCourse(): void {
+    this.courseService.create(this.courseForm.value).subscribe({
       next: course => {
         this.dialogRef.close(course);
       },
       error: error => {
-        console.error('Error creating course: ', error);
+        console.error('Error creating course', error);
       },
     });
   }
 
-  private editCourse(courseData: Course): void {
-    this.course$ = this.courseService.edit(courseData);
-    this.courseSubscription = this.course$.subscribe({
+  private editCourse(): void {
+    this.courseService.edit(this.courseForm.value).subscribe({
       next: course => {
         this.dialogRef.close(course);
       },
--- src/app/modules/app/pages/courses/courses.component.ts
+++ src/app/modules/app/pages/courses/courses.component.ts
@@ -1,34 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { COURSE_HEADERS } from './course.constants';
 import { Course } from 'src/app/shared/models/Courses';
 import { CourseService } from 'src/app/shared/services/course.service';
 import { API_ERROR_MESSAGE } from 'src/app/shared/constants/api.constants';
-import { SNACKBAR_ERROR_DEFAULTS } from 'src/app/shared/constants/snackbar.constants';
+import {
+  SNACKBAR_ERROR_DEFAULTS,
+  SNACKBAR_SUCCESS_DEFAULTS,
+  SnackbarConfig,
+} from 'src/app/shared/constants/snackbar.constants';
 import { DialogService } from 'src/app/shared/layouts/app-layout/services/dialog/dialog.service';
 import { CourseFormComponent } from './components/course-form/course-form.component';
 import { DeleteConfirmationComponent } from 'src/app/shared/components/delete-confirmation/delete-confirmation.component';
 import { ErrorMessageComponent } from 'src/app/shared/components/error-message/error-message.component';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.sass'],
 })
-export class CoursesComponent implements OnInit {
-  headers: string[] = COURSE_HEADERS;
-  data: Course[] = [];
-  isLoading = true;
-  buttonLabel = 'New Course';
+export class CoursesComponent {
+  @ViewChild('sidenav', { static: false }) sidenav: MatSidenav | undefined;
+  public isSidenavOpen = true;
+  public isLoading = true;
+
+  public readonly headers: string[] = COURSE_HEADERS;
+  public data: Course[] = [];
 
   constructor(
-    public courseService: CourseService,
-    private snackBar: MatSnackBar,
-    public readonly dialogService: DialogService
-  ) {}
-
-  ngOnInit() {
+    public readonly courseService: CourseService,
+    private readonly snackBar: MatSnackBar,
+    private readonly dialogService: DialogService
+  ) {
     this.fetchCourses();
   }
 
@@ -48,28 +53,37 @@ export class CoursesComponent implements OnInit {
   private handleError(error: Error): void {
     console.error(API_ERROR_MESSAGE, error);
     this.isLoading = false;
-    this.displaySnackbar(API_ERROR_MESSAGE);
+    this.displaySnackbar(API_ERROR_MESSAGE, SNACKBAR_ERROR_DEFAULTS);
   }
 
-  private displaySnackbar(message: string): void {
+  private displaySnackbar(message: string, customConfig: SnackbarConfig): void {
     this.snackBar.open(
       message,
-      SNACKBAR_ERROR_DEFAULTS.CLOSE_BUTTON_TEXT,
-      SNACKBAR_ERROR_DEFAULTS.CONFIG
+      customConfig.CLOSE_BUTTON_TEXT,
+      customConfig.CONFIG
     );
   }
 
-  showDialogFormComponent(course?: Course): void {
-    const dialogRef$ = this.dialogService.show(
-      CourseFormComponent,
-      course ? course : null
-    );
+  public showFormDialog(course?: Course): void {
+    this.dialogService
+      .show(CourseFormComponent, course ?? null)
+      .subscribe(result => {
+        if (result && course) {
+          this.editCourse(course);
+        } else if (result) {
+          this.createCourse(result);
+        }
+      });
+  }
 
-    dialogRef$.subscribe({
-      next: (result: Course) => {
-        if (result) this.fetchCourses();
-      },
-    });
+  private createCourse({ name }: Course): void {
+    this.displaySnackbar(`Course ${name} created`, SNACKBAR_SUCCESS_DEFAULTS);
+    this.fetchCourses();
+  }
+
+  private editCourse({ name }: Course): void {
+    this.displaySnackbar(`Course ${name} edited`, SNACKBAR_SUCCESS_DEFAULTS);
+    this.fetchCourses();
   }
 
   showDeleteDialog(course: Course) {
