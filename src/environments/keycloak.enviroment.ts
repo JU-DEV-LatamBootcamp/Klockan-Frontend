c56ceaff52114deef97f50b809deaa56f60535ef
Andres Nazzari <andres.nazzari@jala.university>
2024-02-06 16:40:34 -0300

    Update API and Keycloak URLs TG-98

--- src/environments/keycloak.enviroment.ts
+++ src/environments/keycloak.enviroment.ts
@@ -1,10 +1,13 @@
 export const environment = {
   production: false,
-  apiBasePath: `https://localhost:5001/api`,
+  apiBasePath: `${window.location}:5001/api`,
+  // apiBasePath: `https://localhost:5001/api`,
   resourceServerId: 'api',
   keycloak: {
-    issuer: 'http://localhost:8080/realms/Klockan',
-    redirectUri: 'https://localhost:4200/',
+    issuer: `${window.location.origin}:8080/realms/Klockan`,
+    // issuer: 'http://localhost:8080/realms/Klockan',
+    redirectUri: `${window.location.origin}:4200/`,
+    // redirectUri: 'https://localhost:4200/',
     realm: 'Klockan',
     clientId: 'frontend',
     scope: 'openid profile email',
