72a1f382e948850fcc3512b2d3d50c05eceeb650
Andres J. Nazzari <lordcliff@gmail.com>
2024-02-07 11:27:24 -0300

    Add development and production environment configurations

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: false,
+  apiBasePath: `https://localhost:5001/api`,
+  resourceServerId: 'api',
+  keycloak: {
+    issuer: 'http://localhost:8080/realms/Klockan',
+    redirectUri: 'https://localhost:4200/',
+    realm: 'Klockan',
+    clientId: 'frontend',
+    scope: 'openid profile email',
+  },
+  api: {
+    programsEndpoint: '/v1/programs',
+    coursesEndpoint: '/v1/courses',
+    meetingsEndpoint: '/v1/meetings',
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: true,
+  apiBasePath: `https://ec2-3-133-111-214.us-east-2.compute.amazonaws.com:5001/api`,
+  resourceServerId: 'api',
+  keycloak: {
+    issuer: `https://ec2-3-133-111-214.us-east-2.compute.amazonaws.com:8080/realms/Klockan`,
+    redirectUri: `https://ec2-3-133-111-214.us-east-2.compute.amazonaws.com:4200/`,
+    realm: 'Klockan',
+    clientId: 'frontend',
+    scope: 'openid profile email',
+  },
+  api: {
+    programsEndpoint: '/v1/programs',
+    coursesEndpoint: '/v1/courses',
+    meetingsEndpoint: '/v1/meetings',
+  },
+};
