43df2a1571860eda01ddbb4513a8d0df0ac4dd29
Adrian Mendoza Mamani <adrian.mendoza@fundacion-jala.org>
2024-02-08 10:42:48 -0400

    TG-109: Refactor Edition for Program

--- src/app/modules/app/pages/programs/components/program-form/program-form.component.ts
+++ src/app/modules/app/pages/programs/components/program-form/program-form.component.ts
@@ -2,9 +2,7 @@ import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { BaseService } from 'src/app/shared/services/base.service';
 import { Subscription, Observable } from 'rxjs';
-import { ErrorMessageComponent } from 'src/app/shared/components/error-message/error-message.component';
 import { DialogService } from 'src/app/shared/layouts/app-layout/services/dialog/dialog.service';
-import { Program } from 'src/app/shared/models/Programs';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ProgramService } from 'src/app/shared/services/program.service';
@@ -24,29 +22,28 @@ export type EditProgramComponentData<T> = {
     },
   ],
 })
-export class ProgramFormComponent implements OnInit, OnDestroy{ 
+export class ProgramFormComponent implements OnInit, OnDestroy {
   subscription!: Subscription;
   itemEdited$!: Observable<any>;
-  programForm!: FormGroup; 
-  isCreate:boolean = true;
-  headerTypeLabel: string="Create Program";
-  confirmButtonLabel: string="Create";
-  constructor(    
+  programForm!: FormGroup;
+  isCreate = true;
+  headerTypeLabel = 'Create Program';
+  confirmButtonLabel = 'Create';
+  constructor(
     private readonly formBuilder: FormBuilder,
     private readonly dialogRef: MatDialogRef<ProgramFormComponent>,
     private readonly programService: ProgramService,
-    @Inject(MAT_DIALOG_DATA) public data: EditProgramComponentData<any>, 
-    public readonly dialogService: DialogService   
+    @Inject(MAT_DIALOG_DATA) public data: EditProgramComponentData<any>,
+    public readonly dialogService: DialogService
   ) {}
 
   ngOnInit(): void {
     this.initializeForm();
-    if(this.data){      
-      this.headerTypeLabel = "Edit Program";
-      this.confirmButtonLabel = "Editar";
+    if (this.data) {
+      this.headerTypeLabel = 'Edit Program';
+      this.confirmButtonLabel = 'Editar';
       this.isCreate = false;
-    }
-    else{      
+    } else {
       this.isCreate = true;
     }
   }
@@ -57,29 +54,13 @@ export class ProgramFormComponent implements OnInit, OnDestroy{
     }
   }
 
-  onSubmitEdit() {
-    if(this.programForm.controls['name'].errors){
-      this.dialogService.showErrorMessage(ErrorMessageComponent,{title:"Error editing program",detail:"The name is required"});
-      return;
-    }      
-    const { item, service } = this.data;    
-    item.name = this.programForm.value.name;
-    item.description = this.programForm.value.description;    
-    this.itemEdited$ = service.edit(item);
-    this.subscription = this.itemEdited$.subscribe({
-      next: item => {
-        this.dialogRef.close(item);
-      },
-      error: error => {
-        this.dialogRef.close();
-        this.dialogService.show(ErrorMessageComponent);
-      },
-    });
-  }
   private initializeForm(): void {
     this.programForm = this.formBuilder.group({
-      name: [this.data? this.data.item.name:'', [Validators.required, Validators.maxLength(200)]],
-      description: [this.data? this.data.item.description:''],
+      name: [
+        this.data ? this.data.item.name : '',
+        [Validators.required, Validators.maxLength(200)],
+      ],
+      description: [this.data ? this.data.item.description : ''],
     });
   }
 
@@ -95,10 +76,27 @@ export class ProgramFormComponent implements OnInit, OnDestroy{
     return null;
   }
 
+  onSubmitEdit() {
+    if (this.programForm.valid) {
+      const {
+        item: { id },
+      } = this.data;
+      console.log({ id, ...this.programForm.value });
+      this.programService.edit({ id, ...this.programForm.value }).subscribe({
+        next: program => {
+          this.dialogRef.close(program);
+        },
+        error: error => {
+          console.error('Error al crear el programa:', error);
+        },
+      });
+    } else this.programForm.markAllAsTouched();
+  }
+
   public onSubmit(): void {
-    if(!this.isCreate){
+    if (!this.isCreate) {
       this.onSubmitEdit();
-    }else{
+    } else {
       if (this.programForm.valid) {
         this.programService.create(this.programForm.value).subscribe({
           next: program => {
@@ -109,6 +107,6 @@ export class ProgramFormComponent implements OnInit, OnDestroy{
           },
         });
       } else this.programForm.markAllAsTouched();
-    }    
+    }
   }
 }
