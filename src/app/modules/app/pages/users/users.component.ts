a5ed92a785b60624950ce8a6cebcf5804587d878
Andres Nazzari <andres.nazzari@jala.university>
2024-02-19 10:42:08 -0300

    Update users component with new layout and functionality TG-89

--- src/app/modules/app/pages/users/users.component.ts
+++ src/app/modules/app/pages/users/users.component.ts
@@ -1,8 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { API_ERROR_MESSAGE } from 'src/app/shared/constants/api.constants';
+import {
+  SNACKBAR_ERROR_DEFAULTS,
+  SNACKBAR_SUCCESS_DEFAULTS,
+  SnackbarConfig,
+} from 'src/app/shared/constants/snackbar.constants';
 
+import { userTypeColumns, userCommonColumns } from './users.constants';
+import { DialogService } from '../../../../shared/layouts/app-layout/services/dialog/dialog.service';
+import { User } from '../../../../shared/models/User';
+import { ErrorMessageComponent } from 'src/app/shared/components/error-message/error-message.component';
+import { UserService } from '../../../../shared/services/user.service';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.sass'],
 })
-export class UsersComponent {}
+export class UsersComponent {
+  @ViewChild('sidenav', { static: false }) sidenav: MatSidenav | undefined;
+
+  public isSidenavOpen = true;
+  public isLoading = true;
+
+  columns = userTypeColumns;
+  commonColumns = userCommonColumns;
+  users: User[] = [];
+
+  constructor(
+    private readonly dialogService: DialogService,
+    private readonly userService: UserService,
+    public readonly snackBar: MatSnackBar
+  ) {
+    this.fetchUsers();
+  }
+
+  public fetchUsers() {
+    this.isLoading = true;
+    this.userService.getAll().subscribe({
+      next: this.handleSuccess.bind(this),
+      error: this.handleError.bind(this),
+    });
+  }
+  private handleSuccess(users: User[]): void {
+    this.users = users;
+    this.isLoading = false;
+  }
+
+  private handleError(error: Error): void {
+    console.error(API_ERROR_MESSAGE, error);
+    this.isLoading = false;
+    this.displaySnackbar(API_ERROR_MESSAGE, SNACKBAR_ERROR_DEFAULTS);
+  }
+
+  private displaySnackbar(message: string, customConfig: SnackbarConfig): void {
+    this.snackBar.open(
+      message,
+      customConfig.CLOSE_BUTTON_TEXT,
+      customConfig.CONFIG
+    );
+  }
+
+  public showDeleteDialog(item: User): void {
+    return;
+  }
+
+  public showEditDialog(item: User): void {
+    return;
+  }
+}
