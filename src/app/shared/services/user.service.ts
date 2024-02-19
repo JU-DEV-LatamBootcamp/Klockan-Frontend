92d033c214daaf165bb64a0da31c9e49c72c5df5
Andres Nazzari <andres.nazzari@jala.university>
2024-02-19 11:32:57 -0300

    Refactor user service to use dynamic URL

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -14,13 +14,9 @@ export class UserService extends BaseService<User> {
   override getAll(): Observable<User[]> {
     const token = this.oAuthService.getAccessToken();
     const headers = super.createHeaders(token);
-    return this.http.get<User[]>(
-      'https://run.mocky.io/v3/8e98b9bd-f4a8-4a12-870e-ac2744483b46',
-      // this.baseRoute + this.usersPath
-      {
-        headers,
-      }
-    );
+    return this.http.get<User[]>(this.baseRoute + this.usersPath, {
+      headers,
+    });
   }
 
   override create(entity: User): Observable<User> {
