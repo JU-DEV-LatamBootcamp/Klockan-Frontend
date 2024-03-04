287470cb534ecf510dfb013f8a7218085f736a90
Andres Javier Nazzari <151545133+AndresJavierNazzari@users.noreply.github.com>
2024-03-04 13:49:33 -0300

    Refactor formatDate function to use Date object instead of moment.js object (#52)

--- src/app/shared/utils/date-formatter.ts
+++ src/app/shared/utils/date-formatter.ts
@@ -1,5 +1,7 @@
-export function formatDate(date: Date): string {
-  const currentDate = date;
+import * as moment from 'moment';
+
+export function formatDate(date: moment.Moment): string {
+  const currentDate = date.toDate();
   const year = currentDate.getFullYear();
   const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // Adding 1 because months are zero-indexed
   const day = String(currentDate.getDate()).padStart(2, '0');
